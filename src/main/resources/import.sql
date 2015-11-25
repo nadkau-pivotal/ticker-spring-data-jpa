33ce1d6a31f33449420c3b932a9fe7a51ad887b7
nadkau-pivotal <nadkau-pivotal@users.noreply.github.com>
2015-11-25 16:02:44 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,7 +5,7 @@
 -- =================================================================================================
 -- US Common Equities
 insert into Stock(ticker, price) values ('NFLX', 112.62);
-insert into Stock(ticker, price) values ('MS', 35.22);
+insert into Stock(ticker, price) values ('BAC', 17.44);
 insert into Stock(ticker, price) values ('FB', 107.16);
 insert into Stock(ticker, price) values ('TSLA', 226.70);
 
